7dd6eadaf6c53eec527bf8ee46fb65d3ff3f7928
lukasz <lukaszkusa@outlook.com>
2021-01-10 23:50:52 +0100

    IDB

--- demo/src/main/java/com/example/demo/Model/User.java
+++ demo/src/main/java/com/example/demo/Model/User.java
@@ -19,6 +19,9 @@ public class User {
     protected String name;
     protected String lastName;
 
+    public User() {
+    }
+
     public User(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
