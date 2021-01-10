d472f8058634ad2bae1c80fc50242bd54fe793a4
lukasz <lukaszkusa@outlook.com>
2021-01-10 23:47:46 +0100

    IDB

--- demo/src/main/java/com/example/demo/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -6,15 +6,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserRepository userRepository;
 
     @GetMapping("/user")
-    @ResponseBody
     public Iterable<User> getUser(){
         return userRepository.findAll();
     }
