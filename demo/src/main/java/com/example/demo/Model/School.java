ee0a2ff74cfebae71b6e2692e8a9f0dc2e6b00b0
lukasz <lukaszkusa@outlook.com>
2021-01-11 19:18:41 +0100

    Create DB

--- demo/src/main/java/com/example/demo/Model/School.java
+++ demo/src/main/java/com/example/demo/Model/School.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 @Entity
 public class School {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     protected String name;
--- demo/src/main/java/com/example/demo/Model/User.java
+++ demo/src/main/java/com/example/demo/Model/User.java
@@ -11,7 +11,7 @@ import java.util.Set;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     protected String name;
