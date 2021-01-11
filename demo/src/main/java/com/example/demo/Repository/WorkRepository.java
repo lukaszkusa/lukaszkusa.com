d7cc6cee0069989ed27e6ca1a93660ddf3f8550a
lukasz <lukaszkusa@outlook.com>
2021-01-11 19:40:52 +0100

    create DB

---
+++ demo/src/main/java/com/example/demo/Repository/WorkRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.Repository;
+
+
+import com.example.demo.Model.Work;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkRepository extends JpaRepository<Work, Long> {
+}
