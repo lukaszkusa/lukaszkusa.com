6affba2a797c956d4b5d18773eac7b32385f9adc
lukasz <lukaszkusa@outlook.com>
2021-01-11 18:38:51 +0100

    addDB

--- demo/src/main/java/com/example/demo/Model/User.java
+++ demo/src/main/java/com/example/demo/Model/User.java
@@ -1,29 +1,27 @@
 package com.example.demo.Model;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Data
 @Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
     protected String name;
     protected String lastName;
+    protected String email;
+    protected String city;
+    protected String phone;
+    @Column(length = 900)
+    protected String about;
+
+
+    protected String github;
+    protected String linkedin;
+    protected String skype;
 
     public User() {
     }
 
-    public User(String name, String lastName) {
-        this.name = name;
-        this.lastName = lastName;
-    }
 }
