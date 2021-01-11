98930e339f334da7dc3fd4cd133e807b3f788d5f
lukasz <lukaszkusa@outlook.com>
2021-01-11 20:23:33 +0100

    create db

--- demo/src/main/java/com/example/demo/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -4,18 +4,23 @@ import com.example.demo.Model.User;
 import com.example.demo.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserRepository userRepository;
 
-    @GetMapping("/user")
-    public Iterable<User> getUser(){
-        return userRepository.findAll();
+    @GetMapping("/admin")
+    public String getUser(Model model){
+
+        User u = userRepository.getOne(1L);
+        model.addAttribute("user", u);
+
+        return "admin";
     }
 }
